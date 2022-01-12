b8cbcde31cd78b4e77f78cb818590fb5ba161efc
frank Harmsen <frank.Harmsen@ing.com>
2022-01-12 08:32:23 +0100

    added expensecategories part 3  added categorize_for_loop(). + categorize_set_comprehension()

--- budget/Expense.py
+++ budget/Expense.py
@@ -40,3 +40,17 @@ class Expenses():
                     unnecessary_expenses.add(i)
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
+
+    def categorize_set_comphrehension(self):
+        necessary_expense = {x for x in self.list
+                            if  x.category == 'Phone' or x.category == 'Auto and Gas' or
+                                x.category == 'Classes' or x.category == "Utilities" or 
+                                x.category == 'Mortgage'}
+        
+        food_expenses = {x for x in self.list
+                        if x.category == 'Groceries' or x.category == 'Eating Out'}
+        
+        Unnecessary_expenses = set(self.list) - necessary_expense - food_expenses
+
+        return [necessary_expense, food_expenses, Unnecessary_expenses]  
+
